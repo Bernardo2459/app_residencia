18a1edf2ae053e6eb49e543f345e210bcd81bbcf
Wellington Santos <wellingtonsantosceo@gmail.com>
2022-11-22 17:38:56 -0300

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Loader from "./components/Loader";
 
 // function HomeScreen() {
 //   return (
